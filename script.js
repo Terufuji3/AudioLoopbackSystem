229ad2f0e5441eea4e1184b7ace9e873a8050df6
Terufuji3 <terufuji.med@gmail.com>
2024-07-29 22:29:14 +0900

    fix

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
-document.getElementById('start').addEventListener('click', start);
-document.getElementById('stop').addEventListener('click', stop);
+// 開始ボタン
+const startButton = document.getElementById('start');
+
+// 停止ボタン
+const stopButton = document.getElementById('stop');
+
+// タイマー表示
+const timer = document.getElementById('timer');
+
+// リセットボタン
+const resetButton = document.getElementById('reset');
+
+// 開始ボタン押下時
+startButton.addEventListener('click', start);
+
+// 停止ボタン押下時
+stopButton.addEventListener('click', stop);
+
+// リセットボタン押下時
+resetButton.addEventListener('click', reset);
 
 let audioContext;
 let mediaStreamSource;
 let delayNode;
 let mediaRecorder;
 
+// 開始時間
+let startTime;
+// 停止時間
+let stopTime = 0;
+// タイムアウトId
+let timeoutId;
+
+/**
+ * 開始ボタン押下時の処理
+ */
 async function start() {
     const delayTime = parseFloat(document.getElementById('delay').value);
 
@@ -23,10 +51,50 @@ async function start() {
     // Connect nodes
     mediaStreamSource.connect(delayNode);
     delayNode.connect(audioContext.destination);
+
+    startTime = Date.now();
+
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    resetButton.disabled = true;
+
+    // ストップウォッチの表示
+    displayTime();
 }
 
+/**
+ * 停止ボタン押下時の処理
+ */
 function stop() {
     mediaStreamSource.disconnect();
     delayNode.disconnect();
     audioContext.close();
+
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    resetButton.disabled = false;
+
+    clearTimeout(timeoutId);
+    stopTime += Date.now() - startTime;
+}
+
+/**
+ * リセットボタン押下時の処理
+ */
+function reset() {
+    timer.textContent = "00:00:00";
+    stopTime = 0;
+}
+
+/**
+ * 時間を表示する関数
+ */ 
+function displayTime() {
+  const currentTime = new Date(Date.now() - startTime + stopTime);
+  const h = String(currentTime.getHours() - 9).padStart(2, "0");
+  const m = String(currentTime.getMinutes()).padStart(2, "0");
+  const s = String(currentTime.getSeconds()).padStart(2, "0");
+
+  timer.textContent = `${h}:${m}:${s}`;
+  timeoutId = setTimeout(displayTime, 1000);
 }
